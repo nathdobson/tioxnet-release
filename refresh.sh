cd86473a7844973b044c867f81a2b7c3b1c6687a
Nathan Dobson <nadobson@mac.com>
2022-12-19 17:12:16 -0800

    Updated releases

--- refresh.sh
+++ refresh.sh
@@ -3,19 +3,23 @@ set -e
 set -u
 cd ../tioxnet
 BRANCHES=$(git branch --format='%(refname:short)')
-#BRANCHES="v1.3"
 cd ../tioxnet-release
 
-for BRANCH in $BRANCHES; do
-  git rm --cached "$BRANCH" || true
-  rm -rf ./v*/
-  rm -rf ./main/
-done
-rm -rf ./.git/modules/*
-for BRANCH in $BRANCHES; do
-  git config --remove-section submodule.$BRANCH || true
-done
+#for BRANCH in $BRANCHES; do
+#  git rm --cached "$BRANCH" || true
+#  rm -rf ./v*/
+#  rm -rf ./main/
+#done
+#rm -rf ./.git/modules/*
+#for BRANCH in $BRANCHES; do
+#  git config --remove-section submodule.$BRANCH || true
+#done
 
 for BRANCH in $BRANCHES; do
-  git submodule add -b $BRANCH -- https://github.com/nathdobson/tioxnet $BRANCH
+  if [[ ! -d $BRANCH ]]; then
+    git submodule add -b $BRANCH -- https://github.com/nathdobson/tioxnet $BRANCH
+  fi
 done
+
+git add .
+git commit -m "Updated releases"
