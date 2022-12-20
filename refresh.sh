a5c4b941d05194993c469cc13ee7764e6f8c33ee
Nathan Dobson <nadobson@mac.com>
2022-12-19 16:46:49 -0800

    Refresh.

--- refresh.sh
+++ refresh.sh
@@ -2,8 +2,28 @@
 set -e
 set -u
 cd ../tioxnet
-TAGS=$(git tag -l | xargs)
+BRANCHES=$(git branch --format='%(refname:short)')
+BRANCHES="v1.3"
 cd ../tioxnet-release
-for TAG in $TAGS; do
-  git submodule init 
-done
+
+for BRANCH in $BRANCHES; do
+  echo "[submodule \"$BRANCH\"]"
+  echo "  path = $BRANCH"
+  echo "  url = https://github.com/nathdobson/tioxnet"
+done > .gitmodules
+#for BRANCH in $BRANCHES; do
+#  git rm "$BRANCH" || true
+#done
+#
+#rm -rf ./.git/modules/*
+#
+#for BRANCH in $BRANCHES; do
+#  git config --remove-section submodule.$BRANCH || true
+#done
+#
+#for BRANCH in $BRANCHES; do
+#  git submodule add -- https://github.com/nathdobson/tioxnet $BRANCH
+#  cd $BRANCH
+#  git checkout $BRANCH
+#  cd ..
+#done
