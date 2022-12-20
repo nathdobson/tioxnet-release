df613aa964e3bf973d139f6faf07e9444b39c773
Nathan Dobson <nadobson@mac.com>
2022-12-19 16:59:54 -0800

    Add branches.

--- refresh.sh
+++ refresh.sh
@@ -3,29 +3,18 @@ set -e
 set -u
 cd ../tioxnet
 BRANCHES=$(git branch --format='%(refname:short)')
-BRANCHES="v1.3"
+#BRANCHES="v1.3"
 cd ../tioxnet-release
 
-{
-  for BRANCH in $BRANCHES; do
-    echo "[submodule \"$BRANCH\"]"
-    echo "  path = $BRANCH"
-    echo "  url = https://github.com/nathdobson/tioxnet"
-  done
-} > .gitmodules
-#for BRANCH in $BRANCHES; do
-#  git rm "$BRANCH" || true
-#done
-#
-#rm -rf ./.git/modules/*
-#
-#for BRANCH in $BRANCHES; do
-#  git config --remove-section submodule.$BRANCH || true
-#done
-#
-#for BRANCH in $BRANCHES; do
-#  git submodule add -- https://github.com/nathdobson/tioxnet $BRANCH
-#  cd $BRANCH
-#  git checkout $BRANCH
-#  cd ..
-#done
+for BRANCH in $BRANCHES; do
+  git rm --cached "$BRANCH" || true
+  rm -rf ./v*/
+done
+rm -rf ./.git/modules/*
+for BRANCH in $BRANCHES; do
+  git config --remove-section submodule.$BRANCH || true
+done
+
+for BRANCH in $BRANCHES; do
+  git submodule add -b $BRANCH -- https://github.com/nathdobson/tioxnet $BRANCH
+done
